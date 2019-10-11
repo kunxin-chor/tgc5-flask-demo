c8a1da172852c04c76cd9bb8f1d1860dfd49b7f6
Ubuntu <ubuntu@ip-172-31-12-247.ap-southeast-1.compute.internal>
2019-10-11 09:46:30 +0000

    Added flask BMI

---
+++ flask-bmi/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+# the default is the GET method
+@app.route('/bmi')
+def index():
+    return render_template("index.template.html")
+
+# create a route for POST /bmi
+@app.route('/bmi', methods=['POST'])
+def calculate_bmi():
+    # the key we use is the name attribute of the input
+    weight = float(request.form['weight'])
+    height = float(request.form['h'])
+    bmi = weight / (height * height)
+    return "BMI = " + str(bmi)
+    
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
